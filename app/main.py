b3e50edced0ec3ee3b90fa039afabf4440da883b
H P <humanpizzza@gmail.com>
2024-05-31 21:20:21 +0200

    refact

--- app/main.py
+++ app/main.py
@@ -9,9 +9,9 @@ def main():
     # Uncomment this to pass the first stage
     #
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept() # wait for client
-    c, _ = server_socket.accept()  # wait for client
-    c.send(b"HTTP/1.1 200 OK\r\n\r\n")
+    server_socket.accept()[0].sendall(b"HTTP/1.1 200 OK\r\n\r\n")
+    # c, _ = server_socket.accept()  # wait for client
+    # c.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
 
 
 if __name__ == "__main__":
