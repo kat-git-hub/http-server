f9be53ca861088c9d6d8fbb9c0627978721819bf
H P <humanpizzza@gmail.com>
2024-05-31 20:46:12 +0200

    add HTTP 200 OK

--- app/main.py
+++ app/main.py
@@ -10,6 +10,8 @@ def main():
     #
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
     server_socket.accept() # wait for client
+    c, _ = server_socket.accept()  # wait for client
+    c.send(b"HTTP/1.1 200 OK\r\n\r\n")
 
 
 if __name__ == "__main__":
