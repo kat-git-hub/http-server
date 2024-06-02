0dc39dd42d8f457889c2b10f78ba37ada2708541
H P <humanpizzza@gmail.com>
2024-06-02 22:40:30 +0200

    refact v8

--- app/main.py
+++ app/main.py
@@ -1,60 +1,37 @@
 import socket
-import os
+import threading
 import sys
 
 def main():
-    if len(sys.argv) != 3 or sys.argv[1] != '--directory':
-        print("Usage: ./your_server.sh --directory <directory>")
-        return
-
-    directory = sys.argv[2]
-
-    server_socket = socket.create_server(("0.0.0.0", 4221))
-    #server_socket = socket.create_server(("localhost", 4221))
-    server_socket.listen()
-
-    while True:
-        client, _ = server_socket.accept()
-        with client:
-            request = client.recv(1024).decode("utf-8")
-            lines = request.split("\r\n")
-            if not lines:
-                continue
-
-            request_line = lines[0]
-            parts = request_line.split()
-            if len(parts) < 3:
-                continue
-            method, path, _ = parts
-
-
-            if method != 'GET':
-                response = "HTTP/1.1 405 Method Not Allowed\r\n\r\n".encode()
-                client.sendall(response)
-                continue
-
-
-            if path.startswith("/files/"):
-                filename = path[len("/files/"):]
-                file_path = os.path.join(directory, filename)
-
-                if os.path.isfile(file_path):
-
-                    with open(file_path, 'rb') as file:
-                        content = file.read()
-                    content_length = len(content)
-                    response = (
-                        "HTTP/1.1 200 OK\r\n"
-                        "Content-Type: application/octet-stream\r\n"
-                        f"Content-Length: {content_length}\r\n\r\n"
-                    ).encode() + content
-                else:
-                    response = "HTTP/1.1 404 Not Found\r\n\r\n".encode()
-            else:
-                response = "HTTP/1.1 404 Not Found\r\n\r\n".encode()
-
-            client.sendall(response)
-
+    def handle_req(client, addr):
+        data = client.recv(1024).decode()
+        req = data.split('\r\n')
+        path = req[0].split(" ")[1]
+        if path == "/":
+            response = "HTTP/1.1 200 OK\r\n\r\n".encode()  
+        elif path.startswith('/echo'):
+            response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(path[6:])}\r\n\r\n{path[6:]}".encode()
+        elif path.startswith("/user-agent"):
+            user_agent = req[2].split(": ")[1]
+            response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(user_agent)}\r\n\r\n{user_agent}".encode()
+        elif path.startswith("/files"):
+            directory = sys.argv[2]
+            filename = path[7:]
+            print(directory, filename)
+            try:
+                with open(f"/{directory}/{filename}", "r") as f:
+                    body = f.read()
+                response = f"HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: {len(body)}\r\n\r\n{body}".encode()
+            except Exception as e:
+                response = f"HTTP/1.1 404 Not Found\r\n\r\n".encode()
+        else:
+            response = "HTTP/1.1 404 Not Found\r\n\r\n".encode()
+        client.send(response)
+    server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
+    while True:    
+        client, addr = server_socket.accept()
+        threading.Thread(target=handle_req, args=(client, addr)).start()
+    
 
 if __name__ == "__main__":
     main()
