b0f03653b42b1051f918bfeb8eda18628db1cd90
H P <humanpizzza@gmail.com>
2024-06-02 14:00:24 +0200

    add user-agent

--- app/main.py
+++ app/main.py
@@ -18,6 +18,10 @@ def main():
             response = "HTTP/1.1 200 OK\r\n\r\n"
         elif path.startswith("/echo/"):
             response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(path[6:])}\r\n\r\n{path[6:]}"
+        elif path.startswith("/user-agent"):
+            user_agent = data.decode().split("\r\n")[2].split(": ")[1]
+            response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(user_agent)}\r\n\r\n{user_agent}"
+            client.sendall(response.encode())
         else:
             response = "HTTP/1.1 404 Not Found\r\n\r\n"
         client.sendall(response.encode())
