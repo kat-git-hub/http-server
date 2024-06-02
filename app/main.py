5ad1760dc1d9849201437711a28e2351ac754818
H P <humanpizzza@gmail.com>
2024-06-02 15:16:03 +0200

    add user-agent refact v3

--- app/main.py
+++ app/main.py
@@ -19,9 +19,12 @@ def main():
         elif path.startswith("/echo/"):
             response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(path[6:])}\r\n\r\n{path[6:]}"
         elif path.startswith("/user-agent"):
-            user_agent = request[2].split(": ")[1]
+            user_agent = "User-Agent header not found"
+            for line in lines:
+                if line.startswith("User-Agent:"):
+                    user_agent = line.split(": ", 1)[1]
+                    break
             response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(user_agent)}\r\n\r\n{user_agent}"
-            client.sendall(response.encode())
         else:
             response = "HTTP/1.1 404 Not Found\r\n\r\n"
         client.sendall(response.encode())
